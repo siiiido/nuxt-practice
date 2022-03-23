79c273b65e3f096bf100879919c288fba5bd74fb
siiiido <discegaudere24.7@gmail.com>
2022-03-23 20:18:42 +0900

    done

---
+++ components/MovieLoading.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="loading">
+    <span></span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MovieLoading',
+}
+</script>
+
+<style lang="scss" scoped>
+/* Global CSS 적용되어서 따로 설정 안해도 될듯 */
+
+/* .loading {
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  @keyframes circle-loading {
+    to {
+      transform: rotateZ(360deg);
+    }
+  }
+  span {
+    display: block;
+    width: 70px;
+    height: 70px;
+    border-radius: 50%;
+    border: 2px solid transparent;
+    border-top-color: #fff;
+    animation: circle-loading 1200ms ease infinite;
+  }
+} */
+</style>
---
+++ pages/movieAbout.vue
@@ -0,0 +1,11 @@
+<template>
+  <!-- pages 라우팅 테스트 -->
+  <div>about page</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
